cd90b29b5da31742babe05e8333b5d335d1ab57d
johannes <johannes.herman@gmail.com>
2023-09-20 19:31:17 +0200

    implemented naive wave function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <algorithm>
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
@@ -8,28 +9,20 @@
 
 float p = 1.0;
 
-float c[8][3] = {
-    {0.0, 0.0, 0.0},
-    {0.0, p, 0.0},
-    {p, p, 0.0},
-    {p, 0.0, 0.0},
-    {0.0, 0.0, p},
-    {0.0, p, p},
-    {p, p, p},
-    {p, 0.0, p}};
-
-const int tile_amount = 3;
+const int tile_amount = 4;
 
 enum tile_instance {
   air,
+  empty,
   grass,
   water
 };
 
-int connection_amount = 2;
+int connection_amount = 3;
 
 enum connections {
-  empty,
+  air_c,
+  empty_c,
   one
 };
 
@@ -156,6 +149,7 @@ public:
   Tile tiles[size][size][size];
   Tile example_tiles[tile_amount];
   std::vector<tile_instance> possible_tiles[size][size][size];
+  bool collapsed[size][size][size] = {false};
 
   void fillPossibleTiles() {
     for (int x = 0; x < size; x++) {
@@ -179,36 +173,47 @@ public:
       // Set connections
       switch (tile.type) {
       case air: {
-        tile.x_1 = empty;
-        tile.x_0 = empty;
-        tile.y_1 = empty;
-        tile.y_0 = empty;
-        tile.z_1 = empty;
-        tile.z_0 = empty;
+        tile.x_1 = air_c;
+        tile.x_0 = air_c;
+        tile.y_1 = air_c;
+        tile.y_0 = air_c;
+        tile.z_1 = air_c;
+        tile.z_0 = air_c;
+        break;
+      }
+      case empty: {
+        tile.x_1 = empty_c;
+        tile.x_0 = empty_c;
+        tile.y_1 = empty_c;
+        tile.y_0 = empty_c;
+        tile.z_1 = empty_c;
+        tile.z_0 = empty_c;
         break;
       }
       case grass: {
         tile.x_1 = one;
         tile.x_0 = one;
-        tile.y_1 = empty;
-        tile.y_0 = empty;
+        tile.y_1 = air_c;
+        tile.y_0 = empty_c;
         tile.z_1 = one;
         tile.z_0 = one;
 
         polygon_data polygon = {0.2, 0.8, 0.2, {0.0, 0.5, 0.0}, {p, 0.5, 0.0}, {p, 0.5, p}, {0.0, 0.5, p}};
         tile.polygons.push_back(polygon);
-      } break;
+        break;
+      }
       case water: {
         tile.x_1 = one;
         tile.x_0 = one;
-        tile.y_1 = empty;
-        tile.y_0 = empty;
+        tile.y_1 = air_c;
+        tile.y_0 = empty_c;
         tile.z_1 = one;
         tile.z_0 = one;
 
-        polygon_data polygon = {0.2, 0.2, 0.8, {0.0, 1.0, 0.0}, {p, 1.0, 0.0}, {p, 1.0, p}, {0.0, 1.0, p}};
+        polygon_data polygon = {0.2, 0.2, 0.8, {0.0, 0.5, 0.0}, {p, 0.5, 0.0}, {p, 0.5, p}, {0.0, 0.5, p}};
         tile.polygons.push_back(polygon);
-      } break;
+        break;
+      }
       }
 
       // tile.print();
@@ -217,66 +222,100 @@ public:
     }
   }
 
+  // void generateNeighbourList() {
+  //   for (int i = 0; i < tile_amount; i++) {
+  //     Tile *tile = &example_tiles[i];
+  //     if (tile->x_1 == empty_c_c) {
+  //       tile->neighbours_x_1.push_back(air);
+  //     }
+  //     if (tile->x_0 == empty_c) {
+  //       tile->neighbours_x_0.push_back(air);
+  //     }
+  //     if (tile->y_1 == empty_c) {
+  //       tile->neighbours_y_1.push_back(air);
+  //     }
+  //     if (tile->y_0 == empty_c) {
+  //       tile->neighbours_y_0.push_back(air);
+  //     }
+  //     if (tile->z_1 == empty_c) {
+  //       tile->neighbours_z_1.push_back(air);
+  //     }
+  //     if (tile->z_0 == empty_c) {
+  //       tile->neighbours_z_0.push_back(air);
+  //     }
+  //     for (int j = 0; j < tile_amount; j++) {
+  //       Tile *other = &example_tiles[j];
+
+  //       if (tile->x_1 == other->x_0 and tile->x_1 != empty_c) {
+  //         tile->neighbours_x_1.push_back(other->type);
+  //       }
+  //       if (tile->x_0 == other->x_1 and tile->x_0 != empty_c) {
+  //         tile->neighbours_x_0.push_back(other->type);
+  //       }
+  //       if (tile->y_1 == other->y_0 and tile->y_1 != empty_c) {
+  //         tile->neighbours_y_1.push_back(other->type);
+  //       }
+  //       if (tile->y_0 == other->y_1 and tile->y_0 != empty_c) {
+  //         tile->neighbours_y_0.push_back(other->type);
+  //       }
+  //       if (tile->z_1 == other->z_0 and tile->z_1 != empty_c) {
+  //         tile->neighbours_z_1.push_back(other->type);
+  //       }
+  //       if (tile->z_0 == other->z_1 and tile->z_0 != empty_c) {
+  //         tile->neighbours_z_0.push_back(other->type);
+  //       }
+  //     }
+  //     // tile->print();
+  //   }
+  // }
+
   void generateNeighbourList() {
     for (int i = 0; i < tile_amount; i++) {
       Tile *tile = &example_tiles[i];
-      if (tile->x_1 == empty) {
-        tile->neighbours_x_1.push_back(air);
-      }
-      if (tile->x_0 == empty) {
-        tile->neighbours_x_0.push_back(air);
-      }
-      if (tile->y_1 == empty) {
-        tile->neighbours_y_1.push_back(air);
-      }
-      if (tile->y_0 == empty) {
-        tile->neighbours_y_0.push_back(air);
-      }
-      if (tile->z_1 == empty) {
-        tile->neighbours_z_1.push_back(air);
-      }
-      if (tile->z_0 == empty) {
-        tile->neighbours_z_0.push_back(air);
-      }
+
       for (int j = 0; j < tile_amount; j++) {
         Tile *other = &example_tiles[j];
 
-        if (tile->x_1 == other->x_0 and tile->x_1 != empty) {
+        if (tile->x_1 == other->x_0) {
           tile->neighbours_x_1.push_back(other->type);
         }
-        if (tile->x_0 == other->x_1 and tile->x_0 != empty) {
+        if (tile->x_0 == other->x_1) {
           tile->neighbours_x_0.push_back(other->type);
         }
-        if (tile->y_1 == other->y_0 and tile->y_1 != empty) {
+        if (tile->y_1 == other->y_0) {
           tile->neighbours_y_1.push_back(other->type);
         }
-        if (tile->y_0 == other->y_1 and tile->y_0 != empty) {
+        if (tile->y_0 == other->y_1) {
           tile->neighbours_y_0.push_back(other->type);
         }
-        if (tile->z_1 == other->z_0 and tile->z_1 != empty) {
+        if (tile->z_1 == other->z_0) {
           tile->neighbours_z_1.push_back(other->type);
         }
-        if (tile->z_0 == other->z_1 and tile->z_0 != empty) {
+        if (tile->z_0 == other->z_1) {
           tile->neighbours_z_0.push_back(other->type);
         }
       }
-      tile->print();
+      // tile->print();
     }
   }
 
   point findLowestEntropy() {
-    int lowest = tile_amount;
-    point lowest_point = {0, 0, 0};
+    int lowest = 1000;
+    point lowest_point = {-1, -1, -1};
     for (int x = 0; x < size; x++) {
       for (int y = 0; y < size; y++) {
         for (int z = 0; z < size; z++) {
-          if (possible_tiles[x][y][z].size() < tile_amount) {
-            lowest = possible_tiles[x][y][z].size();
-            lowest_point = {x, y, z};
+          if (possible_tiles[x][y][z].size() < lowest) {
+            if (not collapsed[x][y][z]) {
+              lowest = possible_tiles[x][y][z].size();
+              lowest_point = {x, y, z};
+              std::cout << "Lowest point " << x << " " << y << " " << z << std::endl;
+            }
           }
         }
       }
     }
+    collapsed[lowest_point.x][lowest_point.y][lowest_point.z] = true;
     return lowest_point;
   }
 
@@ -285,7 +324,172 @@ public:
     tile_instance pick = poss_tiles->at(rand() % poss_tiles->size());
     poss_tiles->resize(1);
     poss_tiles->at(0) = pick;
+
+    for (Tile pos_tile : example_tiles) {
+      if (pick == pos_tile.type) {
+        tiles[p.x][p.y][p.z] = pos_tile;
+        std::cout << "Picked " << pos_tile.type << std::endl;
+      }
+    }
+
     return pick;
+  }
+
+  std::vector<point> neighbours(point p) {
+    std::vector<point> neighbours;
+    if (p.x - 1 >= 0) {
+      neighbours.push_back({p.x - 1, p.y, p.z});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    if (p.x + 1 < size) {
+      neighbours.push_back({p.x + 1, p.y, p.z});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    if (p.y - 1 >= 0) {
+      neighbours.push_back({p.x, p.y - 1, p.z});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    if (p.y + 1 < size) {
+      neighbours.push_back({p.x, p.y + 1, p.z});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    if (p.z - 1 >= 0) {
+      neighbours.push_back({p.x, p.y, p.z - 1});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    if (p.y + 1 < size) {
+      neighbours.push_back({p.x, p.y, p.z + 1});
+    } else {
+      neighbours.push_back({-1, -1, -1});
+    }
+
+    // for (point p : neighbours) {
+    //   std::cout << p.x << std::endl;
+    //   std::cout << p.y << std::endl;
+    //   std::cout << p.z << std::endl;
+    //   std::cout << std::endl;
+    // }
+    return neighbours;
+  }
+
+  void propagate(point p) {
+    std::vector<point> stack;
+    stack.push_back(p);
+
+    while (stack.size() > 0) {
+      point current_point = stack.back();
+      stack.pop_back();
+
+      std::vector<point> neigh = neighbours(current_point);
+
+      point p_other = neigh[0];
+
+      if (p_other.x != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_x_0) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+      p_other = neigh[1];
+
+      if (p_other.x != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_x_1) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+      p_other = neigh[2];
+
+      if (p_other.y != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_y_0) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+      p_other = neigh[3];
+
+      if (p_other.y != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_y_1) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+      p_other = neigh[4];
+
+      if (p_other.z != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_z_0) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+      p_other = neigh[5];
+
+      if (p_other.y != -1) {
+        std::vector<tile_instance> new_possible_tiles;
+
+        for (tile_instance tile_type_other : possible_tiles[p_other.x][p_other.y][p_other.z]) {
+          for (tile_instance tile_type : tiles[p.x][p.y][p.z].neighbours_z_1) {
+            if (tile_type == tile_type_other) {
+              new_possible_tiles.push_back(tile_type);
+            }
+          }
+        }
+        possible_tiles[p_other.x][p_other.y][p_other.z] = new_possible_tiles;
+      }
+    }
+  }
+
+  void iterate() {
+    while (true) {
+      point p = findLowestEntropy();
+      if (p.x == -1) {
+        std::cout << "BREAK" << std::endl;
+        break;
+      }
+      collapseTileAt(p);
+      propagate(p);
+      std::cout << "loop" << std::endl;
+    }
   }
 
   void setTiles() {
@@ -306,14 +510,46 @@ public:
     generateNeighbourList();
     fillPossibleTiles();
 
-    for (int x = 0; x < size; x++) {
-      for (int y = 0; y < size; y++) {
-        for (int z = 0; z < size; z++) {
-          collapseTileAt({x, y, z});
-          tiles[x][y][z].drawTile();
-        }
-      }
-    }
+    iterate();
+
+    // collapseTileAt({2, 2, 2});
+    // tiles[2][2][2].print();
+
+    // propagate({2, 2, 2});
+
+    // std::cout << "Possible tiles at 1 2 2" << std::endl;
+
+    // for (tile_instance ins : possible_tiles[1][2][2]) {
+    //   std::cout << ins << std::endl;
+    // }
+
+    // collapseTileAt({1, 2, 2});
+    // propagate({1, 2, 2});
+    // tiles[1][2][2].print();
+
+    // collapseTileAt({3, 2, 2});
+    // propagate({3, 2, 2});
+
+    // collapseTileAt({2, 1, 2});
+    // propagate({2, 1, 2});
+
+    // collapseTileAt({2, 3, 2});
+    // propagate({2, 3, 2});
+
+    // collapseTileAt({2, 2, 1});
+    // propagate({2, 2, 1});
+
+    // collapseTileAt({2, 2, 3});
+    // propagate({2, 2, 3});
+
+    // for (int x = 0; x < size; x++) {
+    //   for (int y = 0; y < size; y++) {
+    //     for (int z = 0; z < size; z++) {
+    //       collapseTileAt({x, y, z});
+    //       tiles[x][y][z].drawTile();
+    //     }
+    //   }
+    // }
   }
 
   void drawTiles() {
@@ -339,14 +575,16 @@ void initWorld() {
 
   world.generateWorld();
 
-  point low = world.findLowestEntropy();
-  std::cout << low.x << std::endl;
-  std::cout << low.y << std::endl;
-  std::cout << low.z << std::endl;
-  tile_instance til_ins = world.collapseTileAt(low);
-  std::cout << til_ins << std::endl;
+  // point low = world.findLowestEntropy();
+  // std::cout << low.x << std::endl;
+  // std::cout << low.y << std::endl;
+  // std::cout << low.z << std::endl;
+  // tile_instance til_ins = world.collapseTileAt(low);
+  // std::cout << til_ins << std::endl;
 
-  world.setTiles();
+  // world.setTiles();
+
+  std::cout << std::endl;
 }
 
 void myInit() {
